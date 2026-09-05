357dccbccd287fcf79c837b092cd5893fb3bce1d
Matt Turner <mattst88@gmail.com>
2026-09-05 16:24:57 -0400

    Bump version to 2.0

--- include/config.h
+++ include/config.h
@@ -1,9 +1,9 @@
 #ifndef config_h
 #define config_h
 
-#define ABOOT_MAJOR	0
-#define ABOOT_MINOR	9
-#define ABOOT_VERSION	"1.0_pre20040408"
+#define ABOOT_MAJOR	2
+#define ABOOT_MINOR	0
+#define ABOOT_VERSION	"2.0"
 #define ABOOT_MAGIC	0x30302d746f6f6261 /* "aboot-00" */
 
 #define CONFIG_FILE_PARTITION	1
